523bf355090f1e301500315b58fc10bf4507222b
Juan <juansw03@gmail.com>
2024-09-26 00:07:42 -0500

    feat: ssl certificates added

--- config/server.js
+++ config/server.js
@@ -1,10 +1,22 @@
+const fs = require("fs");
+
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+  },
+  ssl: {
+    key: fs.readFileSync(
+      "/etc/letsencrypt/live/api.anandajaboneria.com/privkey.pem",
+      "utf8"
+    ),
+    cert: fs.readFileSync(
+      "/etc/letsencrypt/live/api.anandajaboneria.com/fullchain.pem",
+      "utf8"
+    ),
   },
 });
